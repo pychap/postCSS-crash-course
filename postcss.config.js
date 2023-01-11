3ad6a05827139452aba333a257aaae0248576539
Peter Chapman <pchapman@ptc.com>
2023-01-11 08:52:29 -0800

    Vars file. @custom selectors. Add postcss plugins

--- postcss.config.js
+++ postcss.config.js
@@ -1,5 +1,13 @@
 module.exports = {
   plugins: [
     require('autoprefixer'),
+    require('postcss-preset-env')({
+      stage: 1,
+    }),
+    require('precss'),
+    require('postcss-import'),
+    require('postcss-assets')({
+      loadPaths: ['dist/img']
+    }),
   ]
 };
